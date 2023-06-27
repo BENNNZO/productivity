c975e97013434529d1dabe5dacdf7292c0dab26d
Benjamin Phillips <benjamingt537@gmail.com>
2023-06-27 19:09:34 -0400

    changed the way it blocks website by making it replace the body with by own html saying the website is blocked then it redirects to google.com after 5 seconds

--- web_blocker/content.js
+++ web_blocker/content.js
@@ -1,54 +1,381 @@
-// const script = document.createElement('script');
-// script.setAttribute("type", "module");
-// script.setAttribute("src", "./main.js");
-// console.log(script)
-// const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-// head.insertBefore(script, head.lastChild);
-
-// import { enabled } from "./script"
-
-// import list of blocked websites
-const sites = ["youtube.com"]
+// import list of a TONNN blocked websites
+const sites = [
+    "43things.com",
+    "4chan.org",
+    "82apps.com",
+    "8ch.net",
+    "8chan.co",
+    "9gag.com",
+    "9to5mac.com",
+    "abcnews.go.com",
+    "addictinggames.com",
+    "addthis.com",
+    "agar.io",
+    "airbnb.com",
+    "albinoblacksheep.com",
+    "allmyfaves.com",
+    "allthingsd.com",
+    "allvid.ch",
+    "alternativeto.net",
+    "amazon.com",
+    "andkon.com",
+    "angel.co",
+    "aniwey.net",
+    "aol.com",
+    "appsandoranges.com",
+    "ardmediathek.de",
+    "arstechnica.com",
+    "artsy.net",
+    "ask.fm",
+    "auroravid.to",
+    "betali.st",
+    "bidvoy.net",
+    "biqle.ru",
+    "bitvid.sx",
+    "blogspot.com",
+    "bloomberg.com",
+    "blowcomics.com",
+    "bo.lt",
+    "boingboing.net",
+    "bored.com",
+    "branch.com",
+    "break.com",
+    "briskfile.com",
+    "bud.ge",
+    "bufferapp.com",
+    "buildandshoot.com",
+    "businessinsider.com",
+    "buy.com",
+    "buzzfeed.com",
+    "cad-comic.com",
+    "camelcamelcamel.com",
+    "canada.com",
+    "candybox2.net",
+    "candystand.com",
+    "cheapassgamer.com",
+    "cheezburger.com",
+    "chime.in",
+    "chrome.google.com",
+    "circleme.com",
+    "cloudtime.to",
+    "cloudy.ec",
+    "collegehumor.com",
+    "comcast.net",
+    "coolrom.com",
+    "cracked.com",
+    "craigslist.org",
+    "crunchbase.com",
+    "crystalmathlabs.com",
+    "cubers.net",
+    "cull.tv",
+    "cultofmac.com",
+    "daclips.in",
+    "dailydot.com",
+    "dailymotion.com",
+    "dashnet.org",
+    "deadspin.com",
+    "delicious.com",
+    "desura.com",
+    "deviantart.com",
+    "devour.com",
+    "digg.com",
+    "divxme.com",
+    "divxstage.to",
+    "docs.google.com",
+    "dpadd.com",
+    "draynor.net",
+    "drive.google.com",
+    "dropvideo.com",
+    "dzone.com",
+    "ebaumsworld.com",
+    "ebay.com",
+    "edition.cnn.com",
+    "eeemo.net",
+    "elderscrollsonline.com",
+    "engadget.com",
+    "entervideo.net",
+    "espn.go.com",
+    "estream.to",
+    "evernote.com",
+    "explosm.net",
+    "extensions",
+    "facebook.com",
+    "fark.com",
+    "feedly.com",
+    "filenuke.com",
+    "flashx.tv",
+    "flixster.com",
+    "fluther.com",
+    "forbes.com",
+    "foundation.bz",
+    "freakshare.com",
+    "funnyjunk.com",
+    "funnyordie.com",
+    "game-oldies.com",
+    "gamepedia.com",
+    "gamovideo.com",
+    "gawker.com",
+    "getglue.com",
+    "getprismatic.com",
+    "gigaom.com",
+    "gizmodo.com",
+    "gocomics.com",
+    "goodvideohost.com",
+    "gorillavid.in",
+    "greevid.com",
+    "hivereader.com",
+    "homestarrunner.com",
+    "huffingtonpost.com",
+    "hulu.com",
+    "hunch.com",
+    "idowatch.net",
+    "ifttt.com",
+    "imgur.com",
+    "indieflix.com",
+    "indiegogo.com",
+    "instagram.com",
+    "invisionfree.com",
+    "io9.com",
+    "itunes.apple.com",
+    "jalopnik.com",
+    "jezebel.com",
+    "jukejuice.com",
+    "justin.tv",
+    "kag2d.com",
+    "kickstarter.com",
+    "kinja.com",
+    "knowyourmeme.com",
+    "koalabeast.com",
+    "kongregate.com",
+    "kotaku.com",
+    "kurzweilai.net",
+    "launch.co",
+    "leetscape.com",
+    "letwatch.us",
+    "lifehacker.com",
+    "livejournal.com",
+    "liveleak.com",
+    "loadingartist.com",
+    "lofog.com",
+    "longform.org",
+    "makeuseof.com",
+    "mashable.com",
+    "medium.com",
+    "metacafe.com",
+    "metafilter.com",
+    "minecraft.net",
+    "minecraftforum.net",
+    "miniclip.com",
+    "minus.com",
+    "movdivx.com",
+    "movpod.in",
+    "movshare.net",
+    "mp4upload.com",
+    "mspaintadventures.com",
+    "myspace.com",
+    "myvideo.de",
+    "nayavideo.com",
+    "neatorama.com",
+    "neodrive.co",
+    "newegg.com",
+    "newgrounds.com",
+    "news.google.com",
+    "news.ycombinator.com",
+    "newsblur.com",
+    "newsle.com",
+    "noowit.com",
+    "noslocker.com",
+    "nosvideo.com",
+    "novamov.com",
+    "nowvideo.ch",
+    "nowvideo.co",
+    "nowvideo.sx",
+    "nowvideo.to",
+    "ok.ru",
+    "oload.tv",
+    "openload.co",
+    "openload.io",
+    "overstock.com",
+    "pandodaily.com",
+    "path.com",
+    "pbfcomics.com",
+    "penny-arcade.com",
+    "pheed.com",
+    "pinboard.in",
+    "pinterest.com",
+    "play.google.com",
+    "playedto.me",
+    "playreplay.net",
+    "plus.google.com",
+    "polygon.com",
+    "popurls.com",
+    "potluck.it",
+    "powvideo.net",
+    "primeshare.tv",
+    "promptfile.com",
+    "purefreetoplay.com",
+    "quora.com",
+    "qz.com",
+    "rapidvideo.com",
+    "rapidvideo.ws",
+    "raptr.com",
+    "raptu.com",
+    "realmofthemadgod.com",
+    "reddit.com",
+    "rottentomatoes.com",
+    "rsbandb.com",
+    "runeclan.com",
+    "runehead.com",
+    "runehints.com",
+    "runehq.com",
+    "runemonkey.net",
+    "runescape.com",
+    "runetrack.com",
+    "runetracker.org",
+    "runeweb.net",
+    "runewise.net",
+    "salmoneus.net",
+    "scape-xp.com",
+    "sendvid.com",
+    "shared.sx",
+    "sharesix.com",
+    "sharethis.com",
+    "slashdot.com",
+    "slashdot.org",
+    "slate.com",
+    "slatestarcodex.com",
+    "slither.io",
+    "smbc-comics.com",
+    "snapzu.com",
+    "snopes.com",
+    "sortable.com",
+    "southparkstudios.com",
+    "speedvid.net",
+    "speedvideo.net",
+    "spring.me",
+    "stagevu.com",
+    "steampowered.com",
+    "stellar.io",
+    "store.maxdome.de",
+    "strawpoll.me",
+    "streamcloud.eu",
+    "streamin.to",
+    "streamplay.to",
+    "stumbleupon.com",
+    "stylee32.net",
+    "swiftirc.net",
+    "teamcoco.com",
+    "teamfortress.com",
+    "techcrunch.com",
+    "techmeme.com",
+    "techvibes.com",
+    "theneeds.com",
+    "thenextweb.com",
+    "theoatmeal.com",
+    "theoldreader.com",
+    "theonion.com",
+    "theverge.com",
+    "thevideo.me",
+    "thevideobee.to",
+    "thevideos.tv",
+    "tip.it",
+    "titanfall.com",
+    "tmz.com",
+    "transformice.com",
+    "tumblr.com",
+    "tweettabs.com",
+    "twitch.tv",
+    "twitter.com",
+    "uptostream.com",
+    "userscloud.com",
+    "userscripts.org",
+    "usersfiles.com",
+    "userstyles.org",
+    "ustream.tv",
+    "valvesoftware.com",
+    "veehd.com",
+    "venturebeat.com",
+    "veoh.com",
+    "versus.com",
+    "vgcats.com",
+    "vice.com",
+    "vid.gg",
+    "vid.me",
+    "vidabc.com",
+    "video.tt",
+    "videosift.com",
+    "videoweed.es",
+    "videowood.tv",
+    "vidgg.to",
+    "vidlox.tv",
+    "vidto.me",
+    "vidtodo.com",
+    "vidup.me",
+    "vidzi.tv",
+    "vimeo.com",
+    "vimple.ru",
+    "virtualnes.com",
+    "vivo.sx",
+    "vk.com",
+    "voat.co",
+    "vodlock.co",
+    "vshare.eu",
+    "vshare.io",
+    "wallbase.cc",
+    "wanelo.com",
+    "watchers.to",
+    "wholecloud.net",
+    "wikia.com",
+    "wikipedia.org",
+    "wimp.com",
+    "wired.com",
+    "wizards.com",
+    "xkcd.com",
+    "xvidstage.com",
+    "yoleoreader.com",
+    "yourvideohost.com",
+    "yourworldoftext.com",
+    "youtube.com",
+    "youwatch.org",
+    "ytmnd.com",
+    "zdf.de",
+    "zetaboards.com",
+    "zifboards.com",
+    "zybez.net",
+    "zynga.com"
+]
 
 chrome.storage.local.get(["toggle"]).then((res) => {
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    console.log("hello world")
-    
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    console.log(res)
-    
-    // check on initial load
-    sites.map(e => {
-        if (window.location.href.includes(e)) window.location.href = "https://www.google.com"
-    })
-    
-    // check again every five seconds unless first check was successfull
-    setInterval(() => {
+    if (res.toggle) {
+        // check on initial load
         sites.map(e => {
-            if (window.location.href.includes(e)) window.location.href = "https://www.google.com"
+            if (window.location.href.includes(e)) {
+                document.body.innerHTML = `
+                    <style>
+                        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap');
+                        * { padding: 0; margin: 0; box-sizing: border-box; font-family: 'Poppins', sans-serif; }
+                        body { display: grid; place-items: center; height: 100vh; background-color: salmon; }
+                        h1 { color: rgb(175, 50, 50); opacity: 0.75; font-size: 10rem; text-align: center; }
+                        p { color: rgb(175, 50, 50); opacity: 0.65; font-size: 2rem; text-align: center; }
+                    </style>
+                    <div>
+                        <h1>BLOCKED</h1>
+                        <p>redirecting to google.com</p>
+                    </div>
+                `
+                document.head.innerHTML = `
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Website Blocked</title>
+                `
+            }
         })
-    }, 5000)
-    
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        console.log(request)
-    })
+
+        // redirect in five seconds
+        setTimeout(() => {
+            sites.map(e => {
+                if (window.location.href.includes(e)) window.location.href = "https://www.google.com"
+            })
+        }, 5000)
+    }
 })
